749f2311a49fecf7b3803a0960003827ada67a4a
kwongsam <kwongsam.85@gmail.com>
2024-05-14 09:02:29 -0700

    Update App.test.tsx
    Fix tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -13,24 +13,24 @@ describe("App", () => {
 
   describe("Conditional rendering", () => {
     // TODO: Step 12
-    it.skip("hides the `Add Task` button when 'Add Task' is clicked", () => {
-      userEvent.click(screen.getByText("Add Task"));
+    it.skip("hides the `Add Task` button when 'Add Task' is clicked", async () => {
+      await userEvent.click(screen.getByText("Add Task"));
 
       expect(screen.queryByText("Add Task")).not.toBeInTheDocument();
     });
 
     // TODO: Step 13
-    it.skip("opens the TaskModal when 'Add Task' is clicked", () => {
-      userEvent.click(screen.getByText("Add Task"));
+    it.skip("opens the TaskModal when 'Add Task' is clicked", async () => {
+      await userEvent.click(screen.getByText("Add Task"));
 
       expect(screen.queryByText("Task Title")).toBeInTheDocument();
     });
 
     // TODO: Step 14
-    it.skip("closes the modal and reveals the `Add Task` button when task is submitted", () => {
-      userEvent.click(screen.getByText("Add Task"));
-      userEvent.type(screen.getByLabelText("Task Title"), "Test Title");
-      userEvent.click(screen.getByText("Submit"));
+    it.skip("closes the modal and reveals the `Add Task` button when task is submitted", async () => {
+      await userEvent.click(screen.getByText("Add Task"));
+      await userEvent.type(screen.getByLabelText("Task Title"), "Test Title");
+      await userEvent.click(screen.getByText("Submit"));
 
       expect(screen.queryByText("Task Title")).not.toBeInTheDocument();
       expect(screen.queryByText("Add Task")).toBeInTheDocument();
@@ -39,10 +39,10 @@ describe("App", () => {
 
   describe("Happy path integration test", () => {
     // TODO: Step 15
-    it.skip("should successfully add tasks to the list", () => {
-      userEvent.click(screen.getByText("Add Task"));
-      userEvent.type(screen.getByLabelText("Task Title"), "AddedTask1");
-      userEvent.click(screen.getByText("Submit"));
+    it.skip("should successfully add tasks to the list", async () => {
+      await userEvent.click(screen.getByText("Add Task"));
+      await userEvent.type(screen.getByLabelText("Task Title"), "AddedTask1");
+      await userEvent.click(screen.getByText("Submit"));
 
       expect(screen.getByText("AddedTask1")).toBeInTheDocument();
     });
